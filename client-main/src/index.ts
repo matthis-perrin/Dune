fc9716d82a531863853cfc4191761ea65c9ac344
Matthis Perrin <matthis.perrin@gmail.com>
2021-11-24 17:50:39 +0100

    Add -tempo parameter for report generation scripts

--- client-main/src/index.ts
+++ client-main/src/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {app, session} from 'electron';
 import log from 'electron-log';
 import fs from 'fs';
@@ -155,3 +156,162 @@ app.on('ready', () => {
 app.on('window-all-closed', () => {
   app.quit();
 });
+=======
+import {app, session} from 'electron';
+import log from 'electron-log';
+import fs from 'fs';
+import {sendEmail} from 'nodejs-nodemailer-outlook';
+import path from 'path';
+
+import {loadConfig} from '@root/config';
+import {prodHoursStore, constantsStore} from '@root/stores';
+import {windowManager} from '@root/window_manager';
+
+import {padNumber} from '@shared/lib/utils';
+import {ClientAppType} from '@shared/models';
+import {AllPromise} from '@shared/promise_utils';
+
+async function startApp(): Promise<void> {
+  if (session.defaultSession) {
+    session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+      callback({responseHeaders: "default-src 'self'"});
+    });
+  }
+  await prodHoursStore.start();
+  await constantsStore.start();
+  await loadConfig();
+  await postStart();
+}
+
+function getArg(name: string): string | undefined {
+  const args = process.argv.slice(1);
+  const matchingArgs = args.filter(arg => arg.startsWith(`${name}=`))[0];
+  // tslint:disable-next-line: strict-type-predicates
+  if (matchingArgs === undefined) {
+    return undefined;
+  }
+  return matchingArgs.slice(name.length + 1);
+}
+
+async function sendEmailAsync(
+  user: string,
+  password: string,
+  dest: string,
+  subject: string,
+  attachments: {filename: string; content: Buffer}[]
+): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    log.info(`Sending email to ${dest}`);
+    sendEmail({
+      auth: {
+        user,
+        pass: password,
+      },
+      from: user,
+      to: dest,
+      subject,
+      attachments,
+      // tslint:disable-next-line: no-any
+      onError: (err: any) => {
+        log.error(err);
+        reject(err);
+      },
+      // tslint:disable-next-line: no-any
+      onSuccess: (data: any) => {
+        log.info(`Success sending to ${dest}`, data);
+        resolve();
+      },
+    });
+  });
+}
+
+async function postStart(): Promise<void> {
+  log.info('Starting with params', process.argv.slice(1));
+  const action = getArg('-action');
+  const archive = getArg('-archive');
+  const user = getArg('-user');
+  const password = getArg('-password');
+  const dest = getArg('-dest');
+  const tempo = parseFloat(getArg('-tempo') ?? '');
+  const tempoChargementUI = Number.isFinite(tempo) ? tempo : 60000;
+  if (
+    action === 'report' &&
+    archive !== undefined &&
+    user !== undefined &&
+    password !== undefined &&
+    dest !== undefined
+  ) {
+    log.info('Mode report. Creating the report window');
+    const reportWindow = await windowManager.openWindow({type: ClientAppType.ReportsPrinterApp});
+    log.info('Created. Waiting a bit...');
+    setTimeout(() => {
+      const archiveDir = path.resolve(archive);
+      const today = new Date();
+      const todayStr = `${today.getFullYear()}-${padNumber(today.getMonth(), 2)}-${padNumber(
+        today.getDate(),
+        2
+      )}`;
+      const filename = `Rapport ${todayStr}.pdf`;
+      let filePath = path.join(archiveDir, filename);
+      log.info(`Will save the report to ${filePath}`);
+      if (fs.existsSync(filePath)) {
+        filePath = path.join(
+          archiveDir,
+          // tslint:disable-next-line: no-magic-numbers
+          `Rapport ${todayStr} ${padNumber(Math.floor(Math.random() * 1000), 4)}.pdf`
+        );
+        log.info(`File already exists. Will save to ${filePath}`);
+      }
+      windowManager
+        .saveAsPDF(reportWindow, filePath, true)
+        .then(data => {
+          const emails = dest.split(',');
+          const subject = `Rapport de production du ${today.toLocaleDateString('fr', {
+            month: 'long',
+            day: '2-digit',
+            year: 'numeric',
+          })}`;
+          const attachments = [
+            {
+              filename: `Rapport ${todayStr}.pdf`,
+              content: data,
+            },
+          ];
+          AllPromise(
+            emails.map(email => sendEmailAsync(user, password, email, subject, attachments))
+          )
+            .then(() => {
+              windowManager.closeWindow(reportWindow.id);
+              process.exit();
+            })
+            .catch(() => {
+              windowManager.closeWindow(reportWindow.id);
+              process.exit();
+            });
+        })
+        .catch(err => {
+          log.error(err);
+          windowManager.closeWindow(reportWindow.id);
+          process.exit();
+        });
+    }, tempoChargementUI);
+
+    return;
+  } else {
+    await windowManager.openWindow({type: ClientAppType.MainApp});
+  }
+}
+
+app.on('ready', () => {
+  startApp()
+    .then(() => log.info('App Started!'))
+    .catch(err => {
+      log.error('Error while starting the app', err);
+      app.quit();
+    });
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+>>>>>>> 89337b5 (Add -tempo parameter for report generation scripts)
